835fb53f83ddc498c9d62bcc1560df91255a982a
okezieuc <c.okezie.u@gmail.com>
2023-08-26 06:55:12 +0000

    create API route to return machine status
    to check the status of a machine, make a request to
    `/api/machine/get` with the query `machine_id=<your_machine_id>`

---
+++ pages/api/machine/get.ts
@@ -0,0 +1,17 @@
+import { kv } from "@vercel/kv";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const machineId = request.query.machine_id;
+  if (machineId == null) {
+    return response
+      .status(404)
+      .json({ error: "please send a machine id with your request" });
+  }
+
+  const machineData = await kv.hgetall("machine:" + machineId);
+  return response.status(200).json(machineData);
+}
